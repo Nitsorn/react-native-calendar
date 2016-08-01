40bbc651260a585f2153610e6b76383cbe63976d
Nitsorn <nitsorn.oat@gmail.com>
2016-08-01 14:28:22 -0700

    buttons styled

--- index.ios.js
+++ index.ios.js
@@ -7,12 +7,15 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  Dimensions,
   Text,
   View,
   TouchableOpacity,
 } from 'react-native';
 
 import Calendar from './calendar';
+
+var {width, height} = Dimensions.get('window');
 
 // const React = require('react-native');
 // let {
@@ -42,20 +45,24 @@ class test extends Component {
   nextMonth() {
     let month = new Date(this.state.month);
     month.setMonth(month.getMonth() + 1);
-    debugger;
     this.setState({month : month});
   }
   render() {
-    debugger;
     return (
       <View style={styles.container}>
         <Text>{this.state.month.getMonth() + 1}</Text>
-        <TouchableOpacity onPress={this.lastMonth.bind(this)}>
-          <Text>Prev</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.nextMonth.bind(this)}>
-          <Text>Next</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity onPress={this.lastMonth.bind(this)}>
+            <View style={styles.buttonWrapper}>
+              <Text style={styles.button}>Prev</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.nextMonth.bind(this)}>
+            <View style={[styles.buttonWrapper,styles.next]}>
+              <Text style={styles.button}>Next</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
         <Calendar month={this.state.month} getDayStyle={this.getDayStyle}/>
       </View>
     );
@@ -79,6 +86,27 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  buttonsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+
+  },
+  buttonWrapper: {
+    width: 0.5*width,
+    alignSelf:'center',
+    height: 50,
+    backgroundColor: '#B0B0B0',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  next: {
+    backgroundColor: '#A3A3A3',
+  },
+  button: {
+    color: 'white',
+    fontSize: 15,
+  }
 });
 
 AppRegistry.registerComponent('test', () => test);
